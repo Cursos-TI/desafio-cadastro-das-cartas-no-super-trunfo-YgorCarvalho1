ced68ae028dd0f907607a86427aace1f8fdc41fc
YgorCarvalho1 <ygorcarvalhoribeiro297@gmail.com>
2025-02-13 16:41:26 +0000

    Terminei

--- xadrez.c
+++ xadrez.c
@@ -4,4 +4,34 @@ int main() {
     printf("Desafio Xadrez!\n");
     printf("Novo commit\n");
 
+    //INFORMAÇOES DAS CARTAS DO TRUNFO
+    char estado;               //LETRAS DO ESTADO (A, B, C...)
+    int cidade;                //NÚMERO DA CIDADE (1 A 4)
+    int populacao;             //POPULAÇÃO DA CIDADE
+    float area;                //ÁREA DA CIDADE EM KM²
+    float pib;                 //PIB DA CIDADE
+    int pontos;                //NÚMERO DE PONTOS TURÍSTICOS
+
+    //BOAS VINDAS AO JOGADOR
+    
+    printf("SUPER TRUNFO DOS PAÍSES");
+    printf("-----------------------");
+
+    //CADASTRO DAS CARTAS
+
+    printf("Digite a letra do estado (A-H):");
+    scanf( %c, &estado);
+    printf("Digite o número da cidade (1-4):");
+    scanf(%d, &cidade);
+    printf("Digite a população da cidade:");
+    scanf(%d, %populacao);
+    printf("Digite a área da cidade (em km²):");
+    scanf(%f, %area);
+    printf("Digite a PIB da cidade:");
+    scanf(%f, %pib);
+    printf("Digite os pontos turísticos da cidade:");
+    scanf(%d, %pontos);
+    )
+
+
 }
